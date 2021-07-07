e65dd7e003cc14bf5f9e8ae64facaea72a4d2eed
ldnicolasmay <ldnicolasmay@gmail.com>
2021-07-07 19:12:37 -0400

    Finish 3.6 List with pop(0) and pop() comparison

--- Chapter03/lists.py
+++ Chapter03/lists.py
@@ -1,26 +1,26 @@
 from timeit import Timer
 
 
-def test0():
+def test0() -> None:
     pass
 
-def test1():
+
+# Appending elements to a list
+
+def test1() -> None:
     l = []
     for i in range(1000):
         l = l + [i]
 
-
-def test2():
+def test2() -> None:
     l = []
     for i in range(1000):
         l.append(i)
 
-
-def test3():
+def test3() -> None:
     l = [i for i in range(1000)]
 
-
-def test4():
+def test4() -> None:
     l = list(range(1000))
 
 
@@ -35,4 +35,24 @@ print(f"concatenation: {t1.timeit(number=1000):15.5f} ms")
 print(f"appending: {t2.timeit(number=1000):19.5f} ms")
 print(f"list comprehension: {t3.timeit(number=1000):10.5f} ms")
 print(f"list range: {t4.timeit(number=1000):18.5f} ms")
+
+
+# Popping elements from a list
+
+list_1 = list(range(2_000_000))
+list_2 = list(range(2_000_000))
+
+pop_zero = Timer("list_1.pop(0)", "from __main__ import list_1")
+pop_end = Timer("list_2.pop()", "from __main__ import list_2")
+
+# print(f"pop(0): {pop_zero.timeit(number=1000):10.5f} ms")
+# print(f"pop(): {pop_end.timeit(number=1000):11.5f} ms")
+
+print(f"{'n':10s}{'pop(0)':>15s}{'pop()':>15s}")
+for i in range(1_000_000, 10_000_001, 1_000_000):
+    list_1 = list(range(i))
+    pop_zero_t = pop_zero.timeit(number=1000)
+    list_2 = list(range(i))
+    pop_end_t = pop_end.timeit(number=1000)
+    print(f"{i:<10d}{pop_zero_t:>15.5f}{pop_end_t:>15.5f}")
 
