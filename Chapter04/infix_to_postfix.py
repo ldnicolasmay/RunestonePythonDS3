8e1d45d4c8d7cb0c87afe553ec686343cf372f6b
ldnicolasmay <ldnicolasmay@gmail.com>
2021-07-13 21:25:34 -0400

    Add infix_to_postfix function using a stack

---
+++ Chapter04/infix_to_postfix.py
@@ -0,0 +1,48 @@
+from typing import List
+
+from stack import Stack
+
+
+def infix_to_postfix(expression: str):# -> List[str]:
+    op_stack = Stack()
+    output_list = []
+    # expression_split = expression.split()
+    operators = {
+        "*": 2, "/": 2,
+        "+": 1, "-": 1,
+        "(": 0, ")": 0,
+    }
+
+    for char in expression.split():
+        # print(char)
+        if char not in operators:
+            output_list.append(char)
+        if char == "(":
+            op_stack.push(char)
+        elif char == ")":
+            while not op_stack.peek() == "(":
+                output_list.append(op_stack.pop())
+            op_stack.pop()
+        elif char in operators:
+            while not op_stack.is_empty() and \
+                    operators[char] < operators[op_stack.peek()]:
+                output_list.append(op_stack.pop())
+            op_stack.push(char)
+
+    while not op_stack.is_empty():
+        output_list.append(op_stack.pop())
+
+    return " ".join(output_list)
+
+
+
+print(f"A + B * C => {infix_to_postfix('A + B * C')}")
+print(f"A * B + C => {infix_to_postfix('A * B + C')}")
+print(f"A * B + C * D => {infix_to_postfix('A * B + C * D')}")
+print(f"A * B + C * D => {infix_to_postfix('A * B + C * D')}")
+print(f"A + B * C + D => {infix_to_postfix('A + B * C + D')}")
+print(
+    f"( A + B ) * C - ( D - E ) * ( F + G ) => "
+    f"{infix_to_postfix('( A + B ) * C - ( D - E ) * ( F + G )')}"
+)
+
