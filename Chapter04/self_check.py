845ce777c3f87437a289a4d3e1fdb203e187e64d
ldnicolasmay <ldnicolasmay@gmail.com>
2021-07-20 21:57:10 -0400

    Add postfix_eval function

---
+++ Chapter04/self_check.py
@@ -0,0 +1,10 @@
+from infix_to_postfix import infix_to_postfix
+from postfix_eval import postfix_eval
+
+
+print(infix_to_postfix("10 + 3 * 5 / ( 16 - 4 )"))
+
+print(postfix_eval("17 10 + 3 * 9 /"))
+
+print(infix_to_postfix("5 * 3 ** ( 4 - 2 )"))
+
