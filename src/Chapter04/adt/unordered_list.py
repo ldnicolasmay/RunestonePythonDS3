ff2463ea8210ff2c21573dca1684c5017c6f9861
ldnicolasmay <ldnicolasmay@gmail.com>
2021-07-31 21:07:11 -0400

    Continue unordered linked list ADT

--- src/Chapter04/adt/unordered_list.py
+++ src/Chapter04/adt/unordered_list.py
@@ -12,10 +12,10 @@ class UnorderedList(object):
     def __str__(self) -> str:
         return str(self.head)
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return self.head is None
 
-    def add(self, item) -> None:
+    def add(self, item: Any) -> None:
         temp_node = Node(item)
         temp_node.next_node = self.head
         self.head = temp_node
@@ -29,7 +29,7 @@ class UnorderedList(object):
 
         return count
 
-    def search(self, item) -> bool:
+    def search(self, item: Any) -> bool:
         current_node = self.head
         while current_node is not None:
             if current_node.data == item:
@@ -38,7 +38,7 @@ class UnorderedList(object):
         
         return False
 
-    def remove(self, item) -> None:
+    def remove(self, item: Any) -> None:
         previous_node = None
         current_node = self.head
 
@@ -55,7 +55,7 @@ class UnorderedList(object):
         else:                                       # data is in non-head
             previous_node.next = current_node.next  # connect previous node to next node
 
-    def append(self, item) -> None:
+    def append(self, item: Any) -> None:
         current_node = self.head
 
         if current_node is None:
@@ -65,7 +65,7 @@ class UnorderedList(object):
                 current_node = current_node.next_node
             current_node.next_node = Node(item)
 
-    def insert(self, pos, item):
+    def insert(self, pos: int, item: Any) -> None:
         previous_node: Node = None
         current_node: Node = self.head
         current_pos = 0
@@ -93,47 +93,138 @@ class UnorderedList(object):
                 previous_node = current_node
                 current_node = current_node.next_node
 
+    def index(self, item: Any) -> int:
+        current_node = self.head
+        current_pos = 0
 
-list0 = UnorderedList()
-print(f"list0={list0}")
-print("... adding node ...")
-list0.add(1)
-print(f"list0={list0}")
-print("... adding node ...")
-list0.add(2)
-print(f"list0={list0}")
-print(f"list0.size()={list0.size()}")
-print(f"list0.search(2)={list0.search(2)}")
-print(f"list0.search(1)={list0.search(1)}")
-print(f"list0.search(0)={list0.search(0)}")
+        while current_node is not None:
+            if current_node.data == item:
+                return current_pos
 
-list1 = UnorderedList()
-print(f"list1={list1}")
-try:
-    list1.remove(1)
-except ValueError as e:
-    print(e)
-print(f"list1={list1}")
-list1.add(1)
-print(f"list1={list1}")
-list1.remove(1)
-print(f"list1={list1}")
-list1.add(1)
-list1.add(2)
-print(f"list1={list1}")
-list1.remove(2)
-print(f"list1={list1}")
+            current_node = current_node.next_node
+            current_pos = current_pos + 1
 
-list2 = UnorderedList()
-list2.append("a")
-print(f"list2={list2}")
-list2.append("b")
-print(f"list2={list2}")
-list2.insert(0, "foo")
-print(f"list2={list2}")
-list2.insert(3, "bar")
-print(f"list2={list2}")
-list2.insert(0, "baz")
-print(f"list2={list2}")
-list2.insert(2, "qux")
-print(f"list2={list2}")
+        return -1
+
+    def pop(self, pos: int = None) -> None:
+        previous_node = None
+        current_node = self.head
+
+        updated_pos = pos if pos else (self.size() - 1)
+
+        if current_node is None:
+            raise ValueError("cannot pop item from empty list")
+
+        current_pos = 0
+        return_val = None
+
+        while current_node is not None:
+            if current_pos == updated_pos:
+                return_val = current_node.data
+                break
+            previous_node = current_node
+            current_node = current_node.next_node
+            current_pos = current_pos + 1
+
+        if current_node is None:
+            raise ValueError(f"`pos` is not in range")
+        elif previous_node is None:
+            self.head = current_node.next_node
+            return return_val
+        else:
+            previous_node.next_node = current_node.next_node
+            return return_val
+
+
+listX = UnorderedList()
+print(f"listX={listX}")
+
+# print(f"listX.pop()={listX.pop()}")
+# print(f"listX={listX}")
+
+listX.append("a")
+print(f"listX={listX}")
+print(listX.pop(0))
+print(f"listX={listX}")
+
+listX.append("a")
+listX.append("b")
+print(f"listX={listX}")
+print(listX.pop(0))
+print(f"listX={listX}")
+
+# print(listX.pop(3))
+# print(f"listX={listX}")
+
+print(listX.pop())
+print(f"listX={listX}")
+
+
+# list0 = UnorderedList()
+# print(f"list0={list0}")
+# print("... adding node ...")
+# list0.add(1)
+# print(f"list0={list0}")
+# print("... adding node ...")
+# list0.add(2)
+# print(f"list0={list0}")
+# print(f"list0.size()={list0.size()}")
+# print(f"list0.search(2)={list0.search(2)}")
+# print(f"list0.search(1)={list0.search(1)}")
+# print(f"list0.search(0)={list0.search(0)}")
+#
+# list1 = UnorderedList()
+# print(f"list1={list1}")
+# try:
+#     list1.remove(1)
+# except ValueError as e:
+#     print(e)
+# print(f"list1={list1}")
+# list1.add(1)
+# print(f"list1={list1}")
+# list1.remove(1)
+# print(f"list1={list1}")
+# list1.add(1)
+# list1.add(2)
+# print(f"list1={list1}")
+# list1.remove(2)
+# print(f"list1={list1}")
+
+# list2 = UnorderedList()
+# list2.append("a")
+# print(f"list2={list2}")
+# list2.append("b")
+# print(f"list2={list2}")
+# list2.insert(0, "foo")
+# print(f"list2={list2}")
+# list2.insert(3, "bar")
+# print(f"list2={list2}")
+# list2.insert(0, "baz")
+# print(f"list2={list2}")
+# list2.insert(2, "qux")
+# print(f"list2={list2}")
+#
+# print(list2.index("baz"))  # expect 0
+# print(list2.index("foo"))
+# print(list2.index("qux"))
+# print(list2.index("a"))
+# print(list2.index("b"))
+# print(list2.index("bar"))
+# print(list2.index("xyzzy"))
+#
+# print(f"list2={list2}")
+# print(list2.pop())
+# print(f"list2={list2}")
+#
+# list3 = UnorderedList()
+# try:
+#     print(list3.pop())
+# except ValueError as e:
+#     print(e)
+# list3.add(1)
+# print(list3.pop())
+# print(f"list3={list3}")
+# list3.add(2)
+# print(f"list3={list3}")
+# print(list3.pop())
+# print(f"list3={list3}")
