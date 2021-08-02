110be8453864c2ee5188e059e8c403d52fbcf94f
ldnicolasmay <ldnicolasmay@gmail.com>
2021-08-01 21:02:55 -0400

    Review unordered link list ADT

--- src/Chapter04/adt/unordered_list.py
+++ src/Chapter04/adt/unordered_list.py
@@ -1,4 +1,3 @@
-import warnings
 from typing import Any
 
 from node import Node
@@ -7,7 +6,7 @@ from node import Node
 class UnorderedList(object):
 
     def __init__(self) -> None:
-        self.head = None
+        self.head: Node = None
 
     def __str__(self) -> str:
         return str(self.head)
@@ -15,83 +14,25 @@ class UnorderedList(object):
     def is_empty(self) -> bool:
         return self.head is None
 
-    def add(self, item: Any) -> None:
-        temp_node = Node(item)
-        temp_node.next_node = self.head
-        self.head = temp_node
-
     def size(self) -> int:
-        current_node = self.head
-        count = 0
+        current_node: Node = self.head
+        current_pos: int = 0
+
         while current_node is not None:
-            count = count + 1
+            current_pos = current_pos + 1
             current_node = current_node.next_node
 
-        return count
+        return current_pos
 
     def search(self, item: Any) -> bool:
-        current_node = self.head
+        current_node: Node = self.head
+
         while current_node is not None:
             if current_node.data == item:
                 return True
             current_node = current_node.next_node
-        
+
         return False
-
-    def remove(self, item: Any) -> None:
-        previous_node = None
-        current_node = self.head
-
-        while current_node is not None:
-            if current_node.data == item:
-                break
-            previous_node = current_node
-            current_node = current_node.next_node
-
-        if current_node is None:                    # reached end; data not found
-            raise ValueError(f"{item} is not in the list")
-        elif previous_node is None:                 # data is in head
-            self.head = current_node.next_node      # connect head to 2nd node
-        else:                                       # data is in non-head
-            previous_node.next = current_node.next  # connect previous node to next node
-
-    def append(self, item: Any) -> None:
-        current_node = self.head
-
-        if current_node is None:
-            self.head = Node(item)
-        else:
-            while current_node.next_node is not None:
-                current_node = current_node.next_node
-            current_node.next_node = Node(item)
-
-    def insert(self, pos: int, item: Any) -> None:
-        previous_node: Node = None
-        current_node: Node = self.head
-        current_pos = 0
-        list_size = self.size()
-
-        if pos > list_size:
-            raise ValueError(f"pos value {pos} is greater than list length")
-        elif pos == 0:
-            temp_node = Node(item)
-            temp_node.next_node = current_node
-            self.head = temp_node
-        else:
-            while current_node is not None:
-                if current_pos == pos:
-                    temp_node = Node(item)
-                    previous_node.next_node = temp_node
-                    temp_node.next_node = current_node
-                    break
-                elif current_pos == list_size - 1:
-                    temp_node = Node(item)
-                    current_node.next_node = temp_node
-                    break
-
-                current_pos = current_pos + 1
-                previous_node = current_node
-                current_node = current_node.next_node
 
     def index(self, item: Any) -> int:
         current_node = self.head
@@ -100,11 +41,72 @@ class UnorderedList(object):
         while current_node is not None:
             if current_node.data == item:
                 return current_pos
-
             current_node = current_node.next_node
             current_pos = current_pos + 1
 
         return -1
+
+    def add(self, item: Any):
+        new_node = Node(item)
+        new_node.next_node = self.head
+        self.head = new_node
+
+    def remove(self, item: Any):
+        previous_node = None
+        current_node: Node = self.head
+
+        if current_node is None:           # empty list
+            raise ValueError("Cannot remove item from empty list")
+
+        while current_node is not None:
+            if current_node.data == item:
+                if previous_node is None:  # only one item in list
+                    self.head = current_node.next_node
+                else:                      # more than one item in list
+                    previous_node.next_node = current_node.next_node
+            previous_node = current_node
+            current_node = current_node.next_node
+
+    def append(self, item: Any):
+        previous_node = None
+        current_node: Node = self.head
+        new_node = Node(item)
+
+        while current_node is not None:
+            previous_node = current_node
+            current_node = current_node.next_node
+
+        if previous_node is None:
+            self.head = new_node
+        else:
+            previous_node.next_node = new_node
+
+    def insert(self, pos: int, item: Any):
+        previous_node = None
+        current_node: Node = self.head
+        current_pos: int = 0
+        new_node = Node(item)
+
+        while current_node is not None:
+            print(f"prev={previous_node}, curr={current_node}")
+            if current_pos == pos:
+                if previous_node is None and current_node is not None:    # at head
+                    self.head = new_node
+                    new_node.next_node = current_node
+                    break
+                else:                                                     # in middle
+                    previous_node.next_node = new_node
+                    new_node.next_node = current_node
+                    break
+            else:
+                previous_node = current_node
+                current_node = current_node.next_node
+            current_pos = current_pos + 1
+
+        if previous_node is None and current_node is None:   # list is empty
+            self.head = new_node
+        if previous_node is not None and current_node is None:
+            previous_node.next_node = new_node
 
     def pop(self, pos: int = None) -> None:
         previous_node = None
@@ -134,97 +136,3 @@ class UnorderedList(object):
         else:
             previous_node.next_node = current_node.next_node
             return return_val
-
-
-listX = UnorderedList()
-print(f"listX={listX}")
-
-# print(f"listX.pop()={listX.pop()}")
-# print(f"listX={listX}")
-
-listX.append("a")
-print(f"listX={listX}")
-print(listX.pop(0))
-print(f"listX={listX}")
-
-listX.append("a")
-listX.append("b")
-print(f"listX={listX}")
-print(listX.pop(0))
-print(f"listX={listX}")
-
-# print(listX.pop(3))
-# print(f"listX={listX}")
-
-print(listX.pop())
-print(f"listX={listX}")
-
-
-# list0 = UnorderedList()
-# print(f"list0={list0}")
-# print("... adding node ...")
-# list0.add(1)
-# print(f"list0={list0}")
-# print("... adding node ...")
-# list0.add(2)
-# print(f"list0={list0}")
-# print(f"list0.size()={list0.size()}")
-# print(f"list0.search(2)={list0.search(2)}")
-# print(f"list0.search(1)={list0.search(1)}")
-# print(f"list0.search(0)={list0.search(0)}")
-#
-# list1 = UnorderedList()
-# print(f"list1={list1}")
-# try:
-#     list1.remove(1)
-# except ValueError as e:
-#     print(e)
-# print(f"list1={list1}")
-# list1.add(1)
-# print(f"list1={list1}")
-# list1.remove(1)
-# print(f"list1={list1}")
-# list1.add(1)
-# list1.add(2)
-# print(f"list1={list1}")
-# list1.remove(2)
-# print(f"list1={list1}")
-
-# list2 = UnorderedList()
-# list2.append("a")
-# print(f"list2={list2}")
-# list2.append("b")
-# print(f"list2={list2}")
-# list2.insert(0, "foo")
-# print(f"list2={list2}")
-# list2.insert(3, "bar")
-# print(f"list2={list2}")
-# list2.insert(0, "baz")
-# print(f"list2={list2}")
-# list2.insert(2, "qux")
-# print(f"list2={list2}")
-#
-# print(list2.index("baz"))  # expect 0
-# print(list2.index("foo"))
-# print(list2.index("qux"))
-# print(list2.index("a"))
-# print(list2.index("b"))
-# print(list2.index("bar"))
-# print(list2.index("xyzzy"))
-#
-# print(f"list2={list2}")
-# print(list2.pop())
-# print(f"list2={list2}")
-#
-# list3 = UnorderedList()
-# try:
-#     print(list3.pop())
-# except ValueError as e:
-#     print(e)
-# list3.add(1)
-# print(list3.pop())
-# print(f"list3={list3}")
-# list3.add(2)
-# print(f"list3={list3}")
-# print(list3.pop())
-# print(f"list3={list3}")
